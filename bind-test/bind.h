137e4e5a0c53ca123ff06ec1a69f773192316877
Andrey Davydov <andrey.a.davydov@gmail.com>
2019-11-18 00:02:05 +0300

    introduce arg_holder

--- bind-test/bind.h
+++ bind-test/bind.h
@@ -1,6 +1,66 @@
 #pragma once
 
+#include <tuple>
+#include <type_traits>
 #include <utility>
+
+namespace detail
+{
+    template<typename Arg>
+    struct simple_arg_holder
+    {
+        Arg arg;
+
+        template<typename... CallArgs>
+        Arg& extract(CallArgs &&...)
+        {
+            return arg;
+        }
+
+        template<typename... CallArgs>
+        Arg const & extract(CallArgs &&...) const
+        {
+            return arg;
+        }
+    };
+
+    template<size_t I>
+    class placeholder;
+
+    template<typename Arg>
+    constexpr bool is_placeholder = false;
+
+    template<size_t I>
+    constexpr bool is_placeholder<placeholder<I>> = true;
+
+    template<typename Arg>
+    struct make_arg_holder : std::conditional<is_placeholder<Arg>, Arg, simple_arg_holder<Arg>> {};
+
+    template<typename Arg>
+    using make_arg_holder_t = typename make_arg_holder<Arg>::type;
+
+    template<>
+    class placeholder<0>
+    {
+    public:
+        template<typename Arg, typename... Rest>
+        static decltype(auto) extract(Arg && arg, Rest && ...)
+        {
+            return std::forward<Arg>(arg);
+        }
+    };
+
+    template<size_t I>
+    class placeholder
+    {
+    public:
+        template<typename Arg, typename... Rest>
+        static decltype(auto) extract(Arg &&, Rest && ... rest)
+        {
+            return placeholder<I - 1>::extract(std::forward<Rest>(rest)...);
+        }
+    };
+}
 
 template<typename F, typename IndexSequence, typename... Args>
 class binder;
@@ -9,29 +69,37 @@ template<typename F, typename... Args, size_t... I>
 class binder<F, std::index_sequence<I...>, Args...>
 {
     F f;
-    std::tuple<Args...> args;
+    std::tuple<detail::make_arg_holder_t<Args>...> args;
 
 private:
-    template<typename Self>
-    static decltype(auto) impl(Self && self)
+    template<typename Self, typename... CallArgs>
+    static decltype(auto) impl(Self && self, CallArgs &&... call_args)
     {
-        return std::forward<Self>(self).f(std::get<I>(std::forward<Self>(self).args)...);
+        return std::forward<Self>(self).f(std::get<I>(std::forward<Self>(self).args).extract(std::forward<CallArgs>(call_args)...)...);
+    }
+
+    template<typename Arg>
+    static auto make_arg_holder(Arg && arg)
+    {
+        return detail::make_arg_holder_t<Arg> { std::forward<Arg>(arg) };
     }
 
 public:
     binder(F && f, Args &&... args)
         : f(std::forward<F>(f))
-        , args(std::forward<Args>(args)...)
+        , args(make_arg_holder(std::forward<Args>(args))...)
     {}
 
-    decltype(auto) operator() ()
+    template<typename... CallArgs>
+    decltype(auto) operator() (CallArgs &&... call_args)
     {
-        return impl(*this);
+        return impl(*this, std::forward<CallArgs>(call_args)...);
     }
 
-    decltype(auto) operator() () const
+    template<typename... CallArgs>
+    decltype(auto) operator() (CallArgs &&... call_args) const
     {
-        return impl(*this);
+        return impl(*this, std::forward<CallArgs>(call_args)...);
     }
 };
 
