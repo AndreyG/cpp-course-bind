fae5c348e27954453e76bb2158d1ca3f220db422
Andrey Davydov <andrey.a.davydov@gmail.com>
2019-11-18 23:44:41 +0300

    motivation: + example with OneShotCallback

--- bind/motivation.cpp
+++ bind/motivation.cpp
@@ -1,6 +1,7 @@
 #include <functional>
 #include <map>
 #include <string>
+#include <memory>
 
 struct Mapper
 {
@@ -15,4 +16,31 @@ int main()
 {
     auto binder = std::bind(Mapper(), 10, std::placeholders::_1);
     binder(10) = "aba";
+}
+
+namespace
+{
+    using namespace std;
+
+    void f(unique_ptr<int>, unique_ptr<int>);
+
+    struct OneShotCallback
+    {
+        unique_ptr<int> ptr1;
+
+        void operator() (unique_ptr<int> ptr2) &&
+        {
+            f(move(ptr1), move(ptr2));
+        }
+    };
+
+    void test()
+    {
+        auto lambda = [c = OneShotCallback(), ptr = make_unique<int>()] () mutable
+        {
+            return std::move(c)(std::move(ptr));
+        };
+        //auto binder = bind(Callable(), make_unique<int>());
+        //move(binder)();
+    }
 }
